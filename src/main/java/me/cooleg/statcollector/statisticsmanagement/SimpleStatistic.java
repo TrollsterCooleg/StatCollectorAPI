bed001fde5aede1ead33bc43fe1c0ded6dff51e8
TrollsterCooleg <git@cooleg.me>
2023-09-10 16:05:00 -0500

    Add SimpleStatistic.java to API

---
+++ src/main/java/me/cooleg/statcollector/statisticsmanagement/SimpleStatistic.java
@@ -0,0 +1,12 @@
+package me.cooleg.statcollector.statisticsmanagement;
+
+public class SimpleStatistic implements Statistic {
+
+    public SimpleStatistic(String name) {}
+
+    @Override
+    public String getName() {
+        return "NAME";
+    }
+
+}

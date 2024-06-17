9d4f49de5ec6252b388408d91893eb326dcbf92f
TrollsterCooleg <git@cooleg.me>
2024-06-17 06:11:33 -0500

    Clean project files, add PluginStateManager

---
+++ src/main/java/me/cooleg/statcollector/PluginStateManager.java
@@ -0,0 +1,15 @@
+package me.cooleg.statcollector;
+
+public class PluginStateManager {
+
+    public static void saveStats() {}
+
+    public static void wipeStats() {}
+
+    public static void saveStatsAndWipe() {}
+
+    public static boolean getCollecting() {return false;}
+
+    public static void setCollecting(boolean collect) {}
+
+}
